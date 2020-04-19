66a44f105f45997dfd3cb2bc6ca5204b176abbda
Ming C <mingc@MingdeMacBook-Pro.local>
2020-04-19 23:54:04 +0800

    xiuagai

--- App/HttpController/Index.php
+++ App/HttpController/Index.php
@@ -5,32 +5,17 @@ namespace App\HttpController;
 
 
 use EasySwoole\Http\AbstractInterface\Controller;
-use EasySwoole\Rpc\Response;
-use EasySwoole\Rpc\Rpc;
 
 class Index extends Controller
 {
 
     public function index()
     {
-        /**$file = EASYSWOOLE_ROOT.'/vendor/easyswoole/easyswoole/src/Resource/Http/welcome.html';
-         * if(!is_file($file)){
-         * $file = EASYSWOOLE_ROOT.'/src/Resource/Http/welcome.html';
-         * }
-         * $this->response()->write(file_get_contents($file));*/
-        $ret = [];
-        $client = Rpc::getInstance()->client();
-        /*
-         * 调用订单列表
-         */
-        $client->addCall('order', 'list', ['page' => 1])
-            ->setOnSuccess(function (Response $response) use (&$ret) {
-                $ret['order'] = $response->toArray();
-            })->setOnFail(function (Response $response) use (&$ret) {
-                $ret['order'] = $response->toArray();
-            });
-        $client->exec(2.0);
-        $this->writeJson(200, $ret);
+        $file = EASYSWOOLE_ROOT . '/vendor/easyswoole/easyswoole/src/Resource/Http/welcome.html';
+        if (!is_file($file)) {
+            $file = EASYSWOOLE_ROOT . '/src/Resource/Http/welcome.html';
+        }
+        $this->response()->write(file_get_contents($file));
     }
 
     protected function actionNotFound(?string $action)
